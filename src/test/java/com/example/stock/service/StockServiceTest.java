791978bc20b4b45a4055cb462ff21af78b2411f8
jiaLEE <jia9510@naver.com>
2023-01-28 00:02:16 +0900

    동시성 이슈 테스트 코드 작성.

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -10,6 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -41,5 +45,27 @@ class StockServiceTest {
 
         Stock stock = stockRepository.findById(1L).orElseThrow();
         Assertions.assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount); // Thread가 다 작업을 끝날때까지 기다린다.
+
+        for(int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        // 100 개 저장 후 반복문을 통해 100 - (1 * 100) = 0 이 될 것으로 예상한다.
+        Assertions.assertEquals(0L, stock.getQuantity());
     }
 }
